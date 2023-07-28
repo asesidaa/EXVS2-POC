9ef049dd27840d11cd722354bb28b9dfb60f21c7
alexia-midgar <onodera_kosaki@icloud.com>
2023-07-28 20:40:37 +0800

    feat(card-rest-api): kick start CardController, create getAll

---
+++ Server/Dto/Response/BareboneCardProfile.cs
@@ -0,0 +1,8 @@
+﻿namespace Server.Dto.Response;
+
+public class BareboneCardProfile
+{
+    public String userName { get; set; } = String.Empty;
+    public String accessCode { get; set; } = String.Empty;
+    public String chipId { get; set; } = String.Empty;
+}
