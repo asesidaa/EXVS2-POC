852d39ba815d739a7d5babd8a9346de39bfbac07
alexia-midgar <onodera_kosaki@icloud.com>
2023-07-29 14:49:51 +0800

    feat(card-rest-api): allow edit of basic card profile

---
+++ WebUI/Shared/Dto/Request/UpdateBasicProfileRequest.cs
@@ -0,0 +1,8 @@
+﻿using WebUI.Shared.Dto.Common;
+
+namespace WebUI.Shared.Dto.Request;
+
+public class UpdateBasicProfileRequest : BasicCardRequest
+{
+    public BasicProfile basicProfile { get; set; }
+}
