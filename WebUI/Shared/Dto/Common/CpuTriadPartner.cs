4202dd29d898c68a47d111c32ef3f46e5ba1f1f2
alexia-midgar <onodera_kosaki@icloud.com>
2023-08-03 00:14:58 +0800

    refactor: turn level related thing to integer

--- WebUI/Shared/Dto/Common/CpuTriadPartner.cs
+++ WebUI/Shared/Dto/Common/CpuTriadPartner.cs
@@ -5,12 +5,12 @@ namespace WebUI.Shared.Dto.Common;
 public class CpuTriadPartner
 {
     public uint MobileSuitId { get; set; } = 0;
-    public uint ArmorLevel { get; set; } = 0;
-    public uint ShootAttackLevel { get; set; } = 0;
-    public uint InfightAttackLevel { get; set; } = 0;
-    public uint BoosterLevel { get; set; } = 0;
-    public uint ExGaugeLevel { get; set; } = 0;
-    public uint AiLevel { get; set; } = 0;
+    public int ArmorLevel { get; set; } = 0;
+    public int ShootAttackLevel { get; set; } = 0;
+    public int InfightAttackLevel { get; set; } = 0;
+    public int BoosterLevel { get; set; } = 0;
+    public int ExGaugeLevel { get; set; } = 0;
+    public int AiLevel { get; set; } = 0;
     public uint Skill1 { get; set; } = 0;
     public uint Skill2 { get; set; } = 0;
     public BurstType BurstType { get; set; } = BurstType.Covering;
