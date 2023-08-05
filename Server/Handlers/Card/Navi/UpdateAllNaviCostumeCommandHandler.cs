3f99fb8f3c63ab734651d8d217e44a1c0798560c
alexia-midgar <onodera_kosaki@icloud.com>
2023-08-05 12:38:50 +0800

    feat: full Navi Costume change

---
+++ Server/Handlers/Card/Navi/UpdateAllNaviCostumeCommandHandler.cs
@@ -0,0 +1,82 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using nue.protocol.exvs;
+using Server.Mappers;
+using Server.Persistence;
+using WebUI.Shared.Dto.Request;
+using WebUI.Shared.Dto.Response;
+using WebUI.Shared.Exception;
+
+namespace Server.Handlers.Card.Navi;
+
+public record UpdateAllNaviCostumeCommand(UpdateAllNaviCostumeRequest Request) : IRequest<BasicResponse>;
+
+public class UpdateAllNaviCostumeCommandHandler : IRequestHandler<UpdateAllNaviCostumeCommand, BasicResponse>
+{
+    private readonly ServerDbContext context;
+
+    public UpdateAllNaviCostumeCommandHandler(ServerDbContext context)
+    {
+        this.context = context;
+    }
+
+    public Task<BasicResponse> Handle(UpdateAllNaviCostumeCommand request, CancellationToken cancellationToken)
+    {
+        var updateAllNaviCostumeRequest = request.Request;
+        
+        var cardProfile = context.CardProfiles
+            .Include(x => x.UserDomain)
+            .FirstOrDefault(x =>
+                x.AccessCode == updateAllNaviCostumeRequest.AccessCode && x.ChipId == updateAllNaviCostumeRequest.ChipId);
+        
+        if (cardProfile == null)
+        {
+            throw new NullReferenceException("Card Profile is invalid");
+        }
+        
+        var user = JsonConvert.DeserializeObject<Response.PreLoadCard.MobileUserGroup>(cardProfile.UserDomain.UserJson);
+        
+        if (user is null)
+        {
+            throw new InvalidCardDataException("User is invalid");
+        }
+
+        var currentGuestNavs = user.GuestNavs;
+        
+        updateAllNaviCostumeRequest.Navis
+            .ForEach(UpsertNavi(currentGuestNavs));
+        
+        cardProfile.UserDomain.UserJson = JsonConvert.SerializeObject(user);
+
+        context.SaveChanges();
+
+        return Task.FromResult(new BasicResponse
+        {
+            Success = true
+        });
+    }
+
+    Action<WebUI.Shared.Dto.Common.Navi> UpsertNavi(List<Response.PreLoadCard.MobileUserGroup.GuestNavGroup> currentGuestNavs)
+    {
+        return navi =>
+        {
+            var guestNavi = currentGuestNavs.FirstOrDefault(guestNaviGroup => guestNaviGroup.GuestNavId == navi.Id);
+
+            if (guestNavi is null)
+            {
+                var newNavi = navi.ToGuestNaviGroup();
+                newNavi.GuestNavSettingFlag = false;
+                newNavi.BattleNavSettingFlag = false;
+                newNavi.GuestNavRemains = 99999;
+                newNavi.BattleNavRemains = 99999;
+                newNavi.GuestNavFamiliarity = 0;
+                newNavi.NewCostumeFlag = false;
+                currentGuestNavs.Add(newNavi);
+                return;
+            }
+
+            guestNavi.GuestNavCostume = navi.CostumeId;
+        };
+    }
+}
