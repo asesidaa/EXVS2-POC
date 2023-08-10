5788259253c39b143ac38d29d71f9cbcfed5cb56
alexia-midgar <onodera_kosaki@icloud.com>
2023-08-10 18:44:44 +0800

    feat: add dto for UploadedImage

---
+++ WebUI/Shared/Dto/Response/UploadedImage.cs
@@ -0,0 +1,8 @@
+﻿namespace WebUI.Shared.Dto.Response;
+
+public class UploadedImage
+{
+    public int ImageId { get; set; }
+    public string Filename { get; set; }
+    public DateTime CreateTime { get; set; } = DateTime.Now;
+}
