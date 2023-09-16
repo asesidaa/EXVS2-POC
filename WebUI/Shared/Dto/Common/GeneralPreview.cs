b76cd4ebdc7abb3560cf5af829d7d75804a6aec9
alexia-midgar <onodera_kosaki@icloud.com>
2023-09-16 22:10:47 +0800

    refactor: preparation for adapting Titles

--- WebUI/Shared/Dto/Common/GeneralPreview.cs
+++ WebUI/Shared/Dto/Common/GeneralPreview.cs
@@ -5,5 +5,7 @@ public class GeneralPreview
     public uint Id { get; set; } = 0;
     public string Value { get; set; } = string.Empty;
     public string Existence { get; set; } = string.Empty;
+    public string? Category { get; set; } = string.Empty;
     public string? Clues { get; set; } = string.Empty;
+    public string? Clues2 { get; set; } = string.Empty;
 }
