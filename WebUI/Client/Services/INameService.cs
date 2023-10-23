74a32d9b863e5352096cbfa7c8bccfa73f027d2c
alexia-midgar <onodera_kosaki@icloud.com>
2023-10-23 22:25:03 +0800

    feat: update name service

--- WebUI/Client/Services/INameService.cs
+++ WebUI/Client/Services/INameService.cs
@@ -7,9 +7,10 @@ namespace WebUI.Client.Services
         public string GetMobileSuitName(uint id);
         public string GetMobileSuitPilotName(uint id);
         public string GetNavigatorName(uint id);
+        public string GetNavigatorSeriesName(uint id);
+        public string GetNavigatorSeiyuuName(uint id);
         public string GetGaugeName(uint id);
         public string? GetLocalizedName(IdValuePair? obj);
-        public string? GetLocalizedPilotName(MobileSuit? obj);
         public string GetCustomizeCommentSentenceName(uint id);
         public string GetCustomizeCommentPhraseName(uint id);
     }
--- WebUI/Client/Services/NameService.cs
+++ WebUI/Client/Services/NameService.cs
@@ -24,12 +24,26 @@ namespace WebUI.Client.Services
             var localizedName = GetLocalizedPilotName(mobilesuit);
             return localizedName ?? "Unknown Pilot";
         }
-
+        
         public string GetNavigatorName(uint id)
         {
             var navigator = _service.GetNavigatorById(id);
             var localizedName = GetLocalizedName(navigator);
             return localizedName ?? "Unknown Navigator";
+        }
+        
+        public string GetNavigatorSeriesName(uint id)
+        {
+            var navigator = _service.GetNavigatorById(id);
+            var localizedName = GetLocalizedNaviSeriesName(navigator);
+            return localizedName ?? "Unknown Series";
+        }
+        
+        public string GetNavigatorSeiyuuName(uint id)
+        {
+            var navigator = _service.GetNavigatorById(id);
+            var localizedName = GetLocalizedNaviSeiyuuName(navigator);
+            return localizedName ?? "Unknown Seiyuu";
         }
 
         public string GetGaugeName(uint id)
@@ -96,7 +110,69 @@ namespace WebUI.Client.Services
 
             // assume value is always filled
             if (string.IsNullOrWhiteSpace(returnString))
-                returnString = obj.Value;
+                returnString = obj.Pilot;
+
+            return returnString;
+        }
+        
+        public string? GetLocalizedNaviSeriesName(Navigator? obj)
+        {
+            if (obj == null)
+                return null;
+
+            var cultureName = Thread.CurrentThread.CurrentCulture.Name;
+
+            // use reflection to get the different properties type.
+            var returnString = string.Empty;
+
+            switch (cultureName)
+            {
+                default:
+                case "en-US":
+                    returnString = obj.Series;
+                    break;
+                case "ja":
+                    returnString = obj.SeriesJP;
+                    break;
+                case "zh-Hans":
+                    returnString = obj.SeriesCN;
+                    break;
+            }
+
+            // assume value is always filled
+            if (string.IsNullOrWhiteSpace(returnString))
+                returnString = obj.Series;
+
+            return returnString;
+        }
+        
+        public string? GetLocalizedNaviSeiyuuName(Navigator? obj)
+        {
+            if (obj == null)
+                return null;
+
+            var cultureName = Thread.CurrentThread.CurrentCulture.Name;
+
+            // use reflection to get the different properties type.
+            var returnString = string.Empty;
+
+            switch (cultureName)
+            {
+                default:
+                case "en-US":
+                    returnString = obj.Seiyuu;
+                    break;
+                case "ja":
+                    returnString = obj.SeiyuuJP;
+                    break;
+                case "zh-Hans":
+                    returnString = obj.SeiyuuCN;
+                    break;
+            }
+
+            // assume value is always filled
+            if (string.IsNullOrWhiteSpace(returnString))
+                returnString = obj.Seiyuu;
 
             return returnString;
         }
