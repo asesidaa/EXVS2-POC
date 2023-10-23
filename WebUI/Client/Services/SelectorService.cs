f6fd13ceab0e33aed5a57c5b804f7f12592a59c8
alexia-midgar <onodera_kosaki@icloud.com>
2023-10-23 22:24:37 +0800

    feat: create new SelectorService

---
+++ WebUI/Client/Services/SelectorService.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq.Expressions;
+using WebUI.Client.Pages;
+
+namespace WebUI.Client.Services;
+
+public class SelectorService : ISelectorService
+{
+    public Expression<Func<CustomizeCard.NaviWithNavigatorGroup, string>> GetNaviSeriesSelector()
+    {
+        var lang = Thread.CurrentThread.CurrentCulture.Name;
+
+        if (lang == "en-US")
+        {
+            return x => x.Navigator.Series;
+        }
+
+        if (lang == "ja")
+        {
+            return x => x.Navigator.SeriesJP;
+        }
+
+        return x => x.Navigator.SeriesCN;
+    }
+    
+    public Expression<Func<CustomizeCard.NaviWithNavigatorGroup, string>> GetNaviSeiyuuSelector()
+    {
+        var lang = Thread.CurrentThread.CurrentCulture.Name;
+
+        if (lang == "en-US")
+        {
+            return x => x.Navigator.Seiyuu;
+        }
+
+        if (lang == "ja")
+        {
+            return x => x.Navigator.SeiyuuJP;
+        }
+
+        return x => x.Navigator.SeiyuuCN;
+    }
+
+    public Expression<Func<CustomizeCard.MobileSuitWithSkillGroup, string>> GetMsPilotSelector()
+    {
+        var lang = Thread.CurrentThread.CurrentCulture.Name;
+
+        if (lang == "en-US")
+        {
+            return x => x.MobileSuit.Pilot;
+        }
+
+        if (lang == "ja")
+        {
+            return x => x.MobileSuit.PilotJP;
+        }
+
+        return x => x.MobileSuit.PilotCN;
+    }
+}
