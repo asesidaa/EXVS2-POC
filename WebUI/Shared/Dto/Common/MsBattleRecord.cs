5e151f22806de36ea65fb1a177c1566ebb5e1df6
alexia-midgar <onodera_kosaki@icloud.com>
2023-10-26 22:07:14 +0800

    feat: api for consolidating battle result

---
+++ WebUI/Shared/Dto/Common/MsBattleRecord.cs
@@ -0,0 +1,8 @@
+﻿namespace WebUI.Shared.Dto.Common;
+
+public class MsBattleRecord
+{
+    public uint MsId { get; set; } = 0;
+    public uint WinCount { get; set; } = 0;
+    public uint LossCount { get; set; } = 0;
+}
