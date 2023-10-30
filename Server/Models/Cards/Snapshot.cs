08d77f22134d93dd22711a2adba0150a272905ec
alexia-midgar <onodera_kosaki@icloud.com>
2023-10-31 00:32:24 +0800

    feat: add new fields and DBs that required for faster consolidation

---
+++ Server/Models/Cards/Snapshot.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Server.Models.Cards;
+
+[Table("snapshot")]
+public class Snapshot : BaseEntity
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; }
+
+    [Required] 
+    public string SnapshotType { get; set; } = string.Empty;
+    
+    [Required] 
+    public string SnapshotData { get; set; } = string.Empty;
+}
---
+++ Server/Persistence/Configurations/SnapshotConfigurations.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Server.Models.Cards;
+
+namespace Server.Persistence.Configurations;
+
+public class SnapshotConfigurations : IEntityTypeConfiguration<Snapshot>
+{
+    public void Configure(EntityTypeBuilder<Snapshot> builder)
+    {
+        builder.HasKey(x => x.Id);
+    }
+}
