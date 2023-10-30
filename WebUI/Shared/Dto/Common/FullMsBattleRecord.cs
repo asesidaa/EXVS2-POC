17fac269399205b3e56bd56c4a02a14e9c4335ab
alexia-midgar <onodera_kosaki@icloud.com>
2023-10-31 00:32:55 +0800

    feat: DTO for storing consolidation results

---
+++ WebUI/Shared/Dto/Common/FullMsBattleRecord.cs
@@ -0,0 +1,10 @@
+﻿namespace WebUI.Shared.Dto.Common;
+
+public class FullMsBattleRecord
+{
+    public uint MsId { get; set; } = 0;
+    public uint OfflineShuffleWinCount { get; set; } = 0;
+    public uint OfflineShuffleLossCount { get; set; } = 0;
+    public uint OfflineTeamWinCount { get; set; } = 0;
+    public uint OfflineTeamLossCount { get; set; } = 0;
+}
---
+++ WebUI/Shared/Dto/Common/FullUsageSnapshot.cs
@@ -0,0 +1,8 @@
+﻿namespace WebUI.Shared.Dto.Common;
+
+public class FullUsageSnapshot
+{
+    public uint CurrentBattleCount { get; set; } = 0;
+    public List<FullBurstTypeUsage> FullBurstTypeUsages { get; set; } = new();
+    public List<FullMsBattleRecord> FullMsBattleRecords { get; set; } = new();
+}
