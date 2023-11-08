19eea643c0a706ad335b545d96c0877aa3ac8863
alexia-midgar <onodera_kosaki@icloud.com>
2023-11-08 20:24:54 +0800

    fix: fix for offline 1 vs 1

---
+++ WebUI/Shared/Dto/Enum/PlayerIndicator.cs
@@ -0,0 +1,8 @@
+﻿namespace WebUI.Shared.Dto.Enum;
+
+public static class PlayerIndicator
+{
+    public static readonly string Player = "Player";
+    public static readonly string Cpu = "Cpu";
+    public static readonly string Discarded = "Discarded";
+}
