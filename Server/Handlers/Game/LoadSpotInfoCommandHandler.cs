ff71f5acab33c6c06ba9e0ee07921a8d6af0f505
alexia-midgar <onodera_kosaki@icloud.com>
2024-02-24 22:12:45 +0800

    feat: remove spot info

--- Server/Handlers/Game/LoadSpotInfoCommandHandler.cs
+++ Server/Handlers/Game/LoadSpotInfoCommandHandler.cs
@@ -14,13 +14,13 @@ public class LoadSpotInfoCommandHandler : IRequestHandler<LoadSpotInfoCommand, R
             Type = request.Request.Type,
             RequestId = request.Request.RequestId,
             Error = Error.Success,
-            load_spot_info = new Response.LoadSpotInfo()
-            {
-                SinfoBgid = 1,
-                SinfoTitle = "Spot Title",
-                SinfoText = "Spot Text",
-                InfbarSwitch = true
-            }
+            // load_spot_info = new Response.LoadSpotInfo()
+            // {
+            //     SinfoBgid = 1,
+            //     SinfoTitle = "Spot Title",
+            //     SinfoText = "Spot Text",
+            //     InfbarSwitch = true
+            // }
         });
     }
 }
