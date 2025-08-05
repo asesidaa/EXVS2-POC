f1ecc4c8094310576240d548b01c5f2f04c9ba22
alexia-midgar <onodera_kosaki@icloud.com>
2025-08-05 18:49:42 +0800

    content update

--- Server-Over/Commands/LoadGameData/ReleasedContentCommand.cs
+++ Server-Over/Commands/LoadGameData/ReleasedContentCommand.cs
@@ -16,10 +16,15 @@ public class ReleasedContentCommand : ILoadGameDataCommand
     {
         var releasedMobileSuits = Enumerable.Range(1, 400).Select(i => (uint)i).ToArray();
         var releasedNavis = Enumerable.Range(1, 51).Select(i => (uint)i).ToArray();
-        var newMobileSuits = _config.GameConfigurations.NewMobileSuits ?? new[] { 322u };
+        var newMobileSuits = _config.GameConfigurations.NewMobileSuits ?? new[] { 135u, 323u, 326u };
         var updatedMobileSuits = _config.GameConfigurations.UpdatedMobileSuits ?? [
-            7u, 92u, 96u, 113u, 126u, 165u, 176u,
-            211u, 222u, 228u, 231u, 284u
+            3u, 7u, 13u, 37u, 54u, 58u,
+            62u, 80u, 92u, 105u, 107u, 108u,
+            113u, 114u, 116u, 121u, 131u, 144u,
+            151u, 157u, 169u, 180u, 181u, 183u,
+            191u, 192u, 194u, 195u, 199u, 209u,
+            222u, 287u, 288u, 291u, 305u, 306u,
+            310u, 313u, 321u, 322u
         ];
 
         loadGameData.ReleaseMsIds = releasedMobileSuits;
